4ee2979d2c78724a4695f1392f1afc6cc12bb89d
kahazdev <thiagorodrigues20045@gmail.com>
2026-05-25 09:54:56 -0300

    Mudanças no Layout, Html e Css do Blog

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -8,3 +8,17 @@ def index(request):
         request,
         "blog/pages/index.html",
     )
+
+
+def post(request):
+    return render(
+        request,
+        "blog/pages/post.html",
+    )
+
+
+def page(request):
+    return render(
+        request,
+        "blog/pages/page.html",
+    )
