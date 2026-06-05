9f52457310a1689923797b43d7054b154f5f7516
kahazdev <thiagorodrigues20045@gmail.com>
2026-06-05 10:17:49 -0300

    Criei a parte interna dos posts

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -23,15 +23,54 @@ def index(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()  # type: ignore
+
     return render(
         request,
         "blog/pages/post.html",
+        {
+            "post": post,
+        }
     )
 
 
-def page(request):
+def page(request, slug):
     return render(
         request,
         "blog/pages/page.html",
+    )
+
+
+def created_by(request, author_pk):
+    posts = (Post.objects.get_published()  # type: ignore
+             .filter(created_by__pk=author_pk))
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = (Post.objects.get_published()  # type: ignore
+             .filter(category__slug=slug))
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        }
     )
