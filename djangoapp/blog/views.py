3ef65d866db670975ef819c7ec6f2e7ce6b8608c
kahazdev <thiagorodrigues20045@gmail.com>
2026-06-16 11:49:40 -0300

    Criei o Pages e adicionei o sobre mim

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from blog.models import Post
+from blog.models import Post, Page
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -36,9 +37,18 @@ def post(request, slug):
 
 
 def page(request, slug):
+    page = (
+        Page.objects.filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+
     return render(
         request,
         "blog/pages/page.html",
+        {
+            "page": page,
+        }
     )
 
 
@@ -89,5 +99,26 @@ def tag(request, slug):
         "blog/pages/index.html",
         {
             "page_obj": page_obj,
+        }
+    )
+
+
+def search(request):
+    search_value = request.GET.get("search", "").strip()
+
+    posts = (Post.objects.get_published()  # type: ignore
+             .filter(
+                 Q(title__icontains=search_value) |
+                 Q(excerpt__icontains=search_value) |
+                 Q(content__icontains=search_value)
+    )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": posts,
+            "search_value": search_value,
         }
     )
