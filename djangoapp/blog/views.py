a514140865d15feb09b016d822d22cbd6fca1c25
kahazdev <thiagorodrigues20045@gmail.com>
2026-06-22 11:31:33 -0300

    Troquei minha function based view index por uma class based view

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,13 +1,41 @@
+from typing import Any
+
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404
+from django.views.generic import ListView
 
 PER_PAGE = 9
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/pages/index.html"
+    context_object_name = "posts"
+    ordering = "-pk",
+    paginate_by = PER_PAGE
+    queryset = Post.objects.get_published()  # type: ignore
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(is_published=True)
+    #     return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update(
+            {
+                "page_title": "Home - "
+            }
+        )
+
+        return context
 
 
 def index(request):
